0fd4baaa78e09f4829b6895c017a6b7adeb2b763
uku <uku3lig@gmail.com>
2023-02-17 18:27:17 +0100

    update fish & starship

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,12 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+function fish_greeting
+    nofetch
 end
 
-function fish_greeting
-    cowsay $(fortune -s)
-end
+set -x PATH $HOME/.cargo/bin /home/leo/.local/bin $PATH
+
+fish_ssh_agent
+
+alias vim="nvim"
 
 starship init fish | source
 
