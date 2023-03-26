c0f4d4a11d22b7bc6dec12226ba5e436c63e77f7
uku <uku3lig@gmail.com>
2023-03-26 01:06:01 +0100

    launch hyprland on login

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,11 +2,15 @@ function fish_greeting
     nofetch
 end
 
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec Hyprland
+    end
+end
+
 set -x PATH $HOME/.cargo/bin /home/leo/.local/bin $PATH
 
-fish_ssh_agent
-
 alias vim="nvim"
+set -x EDITOR vim
 
 starship init fish | source
-
